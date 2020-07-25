1ee228a0fc3b737001d44b5dfa9b5e766e33919b
Denzell Grant <denzgrant@gmail.com>
2020-07-24 19:15:38 -0700

    pushing to heroku

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,27 +6,27 @@ import portfolios from "./portfolio.json"
 const Portfolio = (props) => {
   return (
     <>
-    <MDBCol>
-      <MDBCard style={{ width: "22rem" }}>
-        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-        <MDBCardBody>
-<MDBCardTitle>{props.name}</MDBCardTitle>
-          <MDBCardText>
-            {props.description}
-          </MDBCardText>
-          <MDBBtn href="#">MDBBtn</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
-    {portfolios.map(portfolio => (
-      <Portfolio
-      name={portfolio.name}
-      image={portfolio.image}
-      description={portfolio.description}
-      github={portfolio.github}
-      url={portfolio.url}
-      />
-    ))}
+      <MDBCol>
+        <MDBCard style={{ width: "22rem" }}>
+          <MDBCardImage className="img-fluid" src={props.image} waves />
+          <MDBCardBody>
+            <MDBCardTitle>{props.name}</MDBCardTitle>
+            <MDBCardText>
+              {props.description}
+            </MDBCardText>
+            <MDBBtn href="#">{props.url}</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      {portfolios.map(port => (
+        <port
+          name={port.name}
+          image={port.image}
+          description={port.description}
+          github={port.github}
+          url={port.url}
+        />
+      ))}
     </>
   )
 }
