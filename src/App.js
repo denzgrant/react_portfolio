41785cc75850a490c4e9acdafd5331062bad2500
Denzell Grant <denzgrant@gmail.com>
2020-07-28 22:35:18 -0700

    removed firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 // import NavTabs from "./components/NavTabs";
 import Home from "./components/Home/Home";
 import Portfolio from "./components/Portfolio/Portfolio";
