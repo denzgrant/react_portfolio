50a98a776796c7ff8b9ab8a38a5a1d679f4cff28
Denzell Grant <denzgrant@gmail.com>
2020-08-20 21:56:58 -0700

    updated cards and buttons

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React , { useEffect } from 'react';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBContainer, MDBRow } from 'mdbreact';
 import './Portfolio.css'
 import projects from '../../projects.json'
+import Aos from "aos"; 
+import "aos/dist/aos.css"
 
 
 
 const Portfolio = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 }); 
+  }, [])
   return (
     <>
       <MDBContainer >
         <MDBRow >
           {projects.map((proj, index) => (
             <MDBCol key={index} sm="6" md="6">
-              <MDBCard className="cards">
+              <MDBCard data-aos="zoom-in" className="cards">
                 <MDBCardImage className="img-fluid" src={process.env.PUBLIC_URL + proj.image} />
                 <MDBCardBody>
                   <MDBCardTitle>{proj.name}</MDBCardTitle>
